68a64613b934cbc8c9ecb896fb4d55f559789c1b
BenArtzi4 <gal.ben.artzi.shachar@intel.com>
2022-12-28 21:14:12 +0200

    add score reset function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Get elements to variables
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+
+resetScore();
+
+function resetScore() {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+}
